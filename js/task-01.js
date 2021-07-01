712b0126144eb0dba70b9535d869535d87fca4e1
Dimon-Stasyuk <78039077+Dimon-Stasyuk@users.noreply.github.com>
2021-07-01 18:11:41 +0300

    сделал дз-1

---
+++ js/task-01.js
@@ -0,0 +1,20 @@
+const allEl = document.querySelectorAll('.item')
+const firstEl = allEl[0].querySelectorAll('li')
+const secondEl = allEl[1].querySelectorAll('li')
+const thirdEl = allEl[2].querySelectorAll('li')
+
+const firstTitle = allEl[0].querySelector('h2')
+const secondTitle = allEl[1].querySelector('h2')
+const thirdTitle = allEl[2].querySelector('h2')
+
+
+console.log(`Количество категорий: ${allEl.length}`)
+
+console.log(`Категория: ${firstTitle.textContent}`)
+console.log(`Количество элементов: ${firstEl.length}`)
+
+console.log(`Категория: ${secondTitle.textContent}`)
+console.log(`Количество элементов: ${secondEl.length}`)
+
+console.log(`Категория: ${thirdTitle.textContent}`)
+console.log(`Количество элементов: ${thirdEl.length}`)
---
+++ js/task-02.js
@@ -0,0 +1,27 @@
+const ingredients = [
+  'Картошка',
+  'Грибы',
+  'Чеснок',
+  'Помидоры',
+  'Зелень',
+  'Приправы',
+];
+
+
+const potatoEl = document.createElement('li')
+potatoEl.textContent = ingredients[0]
+
+const mushroomsEl = document.createElement('li')
+mushroomsEl.textContent = ingredients[1]
+
+const garlicEl = document.createElement('li')
+garlicEl.textContent = ingredients[2]
+
+const tomatoesEl = document.createElement('li')
+tomatoesEl.textContent = ingredients[3]
+
+const greensEl = document.createElement('li')
+greensEl.textContent = ingredients[4]
+
+const condimentsEl = document.createElement('li')
+condimentsEl.textContent = ingredients[5]
